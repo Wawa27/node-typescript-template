8512fd1e8fa75eeb797417c53e8baef530db6dfe
Wawa27 <Oualid.hassan.pro@gmail.com>
2022-10-19 18:52:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-typescript template
+
+This is a template for creating a Node.js project using TypeScript.
---
+++ tests/test.spec.ts
@@ -0,0 +1,7 @@
+import assert from "assert";
+
+describe("First test", () => {
+    it("should pass", () => {
+        assert.ok(true);
+    });
+});
